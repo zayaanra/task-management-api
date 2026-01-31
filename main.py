f024d9937125abda83a61124bb92fb64d3e14f83
Zayaan <rahmanzayaan43@gmail.com>
2026-01-30 22:20:23 -0500

    add update and delete

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from datetime import datetime
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-task_id = 0
 class Task:
     def __init__(self, id: int, title: str, completed: bool, created_at: str):
         self.id = id
@@ -12,8 +12,58 @@ class Task:
 
 tasks = []
 
-
-@app.route("/tasks", methods=['POST'])
+@app.route("/tasks", methods=['GET', 'POST'])
 def create_task():
+    if request.method == 'POST':
+        content = request.json
+
+        # check if POST request body is valid
+        if 'title' not in content or 'completed' not in content:
+            return Response(status=400)
+        
+        # check for duplicate records
+        for task in tasks:
+            if task.title == content['title']:
+                return Response(status=403)
+        
+        task = Task(len(tasks), content['title'], content['completed'], datetime.now().strftime('%d%m%Y_%H:%M:%S'))
+        tasks.append(task)
+
+        return Response(status=201)
+    
+    completed = request.args.get('completed')
+    if completed is None:
+        return {'tasks': tasks}
+    
+    filtered_tasks = []
+    for task in tasks:
+        if bool(completed) == task.completed:
+            filtered_tasks.append({'title': task.title, 'completed': task.completed})
+
+    return {'tasks': filtered_tasks}
+
+@app.route("/tasks/<id>", methods=['DELETE', 'PATCH'])
+def update_task(id):
     content = request.json
+
+    id = int(id)
+    if id < 0 or id >= len(tasks):
+        return Response(status=404)
+    
+    if request.method == 'PATCH':
+        # title and completed not present - body is malformed
+        if 'title' not in content and 'completed' not in content:
+            return Response(status=400)
+        
+        if 'title' in content:
+            tasks[id].title = content['title']
+        
+        if 'completed' in content:
+            tasks[id].completed = content['completed']
+
+        return Response(status=200)
+    
+    if request.method == 'DELETE':
+        del tasks[id]
+        return Response(status=204)
     
